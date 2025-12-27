f62b9b8399935ecb6e4e6b4872a809c4d6e11e04
Michael Tinsley <27818609+michaeltinsley@users.noreply.github.com>
2025-12-27 17:27:07 +0000

    feat: update fish config (#10)

---
+++ fish/.config/fish/alias.fish
@@ -0,0 +1,11 @@
+alias ls=lsd
+
+# ls
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+
+# misc
+alias reload='exec fish'
+alias brew-backup="brew bundle dump --force --file=~/.config/Brewfile"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,41 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
-starship init fish | source
-op completion fish | source  # 1Password CLI
 
-# uv
-fish_add_path "$HOME/.local/bin"
-uv generate-shell-completion fish | source
-uvx --generate-shell-completion fish | source
+if status is-interactive
+    source ~/.config/fish/alias.fish
+
+    # Configure Jump
+    source (jump shell fish | psub)
+
+    # Fish syntax highlighting
+    set -g fish_color_autosuggestion '555'  'brblack'
+    set -g fish_color_cancel -r
+    set -g fish_color_command --bold
+    set -g fish_color_comment red
+    set -g fish_color_cwd green
+    set -g fish_color_cwd_root red
+    set -g fish_color_end brmagenta
+    set -g fish_color_error brred
+    set -g fish_color_escape 'bryellow'  '--bold'
+    set -g fish_color_history_current --bold
+    set -g fish_color_host normal
+    set -g fish_color_match --background=brblue
+    set -g fish_color_normal normal
+    set -g fish_color_operator bryellow
+    set -g fish_color_param cyan
+    set -g fish_color_quote yellow
+    set -g fish_color_redirection brblue
+    set -g fish_color_search_match 'bryellow'  '--background=brblack'
+    set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+    set -g fish_color_user brgreen
+    set -g fish_color_valid_path --underline
+
+    starship init fish | source
+    op completion fish | source  # 1Password CLI
+
+    # uv
+    fish_add_path "$HOME/.local/bin"
+    uv generate-shell-completion fish | source
+    uvx --generate-shell-completion fish | source
+end
